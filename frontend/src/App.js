13a722ec8fd6dfe52218ece0db677075fc6fee93
AVPavel <andreipavel56@gmail.com>
2024-11-20 19:11:57 +0200

    Some tests for the .svg files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Login from "./components/Login/Login";
+import myPhoto from "./assets/images/LOGO_artmap-01.svg"
 
 function App() {
   return (
-    <div className="App">
-        <Login/>
-    </div>
+      <div className="App">
+          <Login/>
+          <img src={myPhoto} alt="Artmap Logo" style={{maxWidth: "1200px"}}/>
+      </div>
   );
 }
 
