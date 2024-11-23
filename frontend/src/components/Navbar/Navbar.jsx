bdaaaaa9ecfd2d038f1e98b2f84b5bb4e537f36e
AVPavel <andreipavel56@gmail.com>
2024-11-23 21:54:18 +0200

    Added V1 of Navigation bar for home page

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,50 @@
 import React from "react";
 import styles from "./Navbar.module.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Logo from "../../assets/images/LOGO_artmap.svg";
 
-const Navbar = () =>{
+const Navbar = () => {
     return (
         <nav className={styles.navbar}>
+            <div className={styles.navSection}>
+                <ul className={styles.navLinks}>
+                    <li>
+                        <Link to="/">Messages</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Recommendations</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Favorites</Link>
+                    </li>
+                </ul>
+            </div>
             <div className={styles.logoContainer}>
                 <Link to="/">
                     <img src={Logo} alt="logo" className={styles.logoImage}/>
                 </Link>
             </div>
-            <ul className={styles.navLinks}>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/Login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/Register">Register</Link>
-                </li>
-            </ul>
+            <div className={styles.navSection}>
+                <ul className={styles.navLinks}>
+                    <li>
+                        <Link to="/">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/Contact">Contact</Link> {/* Updated path */}
+                    </li>
+                    <li>
+                        <Link to="/">News</Link>
+                    </li>
+                    <li>
+                        <Link to="/Login">
+                            <button className={styles.signInButton}>Sign in</button>
+                        </Link> {/* Capitalized for consistency */}
+                    </li>
+                </ul>
+            </div>
         </nav>
-    )
-}
+
+    );
+};
 
 export default Navbar;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,12 +6,12 @@ import Navbar from "../../components/Navbar/Navbar";
 const Home = () =>{
     return (
         <div className={styles.container}>
-            <Navbar/>
-            <h1>Hello BIBI din nou</h1>
-            <p>This is the home page</p>
-            <nav>
-                <Link to="/login">Login</Link> | <Link to="/register">Register</Link>
-            </nav>
+            <div>
+                <Navbar/>
+            </div>
+            <div>
+
+            </div>
         </div>
 
     )
